c3236d52eb701dc08999221a19e6dc941f7b4bce
Xing Su <xsu1989@outlook.com>
2020-02-18 20:56:26 +0800

    A complete rewritten

--- textualmindmap.py
+++ textualmindmap.py
@@ -22,26 +22,27 @@ class AnonymousAttributeError(Exception):
     return str(node)
 
 class Node:
-  tmmnodepattern = r'''(\(\s*(?P<id>[\w\.]+)\s*\))?  # (optional) parentheses enclosed id
-                       \s*                         # (optional) sperating spaces
-                       (\[\s*(?P<label>[\w\.]+)\s*\])?# (optional) brakets enclosed label
-                       \s*                         # (optional) sperating spaces
-                       (?P<text>.+)                # (requried) node text'''
-  tmmnoderegex = re.compile(tmmnodepattern, re.VERBOSE)
+  tmmpattern = r'''
+  (\((?P<id>[\w\.]+)\))?      # (optional) parentheses enclosed id
+  \s*                         # (optional) sperating spaces
+  (\[(?P<tags>[\w\.,]+)\])?   # (optional) brakets enclosed tags
+  \s*                         # (optional) sperating spaces
+  (?P<text>.+)                # (requried) node text'''
+  tmmregex = re.compile(tmmpattern, re.VERBOSE)
   def __init__(self, text):
-    match = Node.tmmnoderegex.fullmatch(text)
+    match = self.tmmregex.fullmatch(text)
     if not match:
       raise InvalideSyntaxError()
     self._text = match.group('text')
     self._identifier = match.group('id')
-    self._label = match.group('label')
+    self._tags = set()
+    if match.group('tags'):
+      self._tags = match.group('tags').split(',')
     self._parent = None
     self._children = []
-    self._level = -1
-    self._index = -1
+    self._level = 0
+    self._index = 0
     self._attributes = dict()
-    self._visible = True
-    self.graph=None
   def __str__(self):
     return self._text
   # getters
@@ -49,71 +50,59 @@ class Node:
     return self._text;
   def identifier(self):
     return self._identifier;
-  def label(self):
-    return self._label;
+  def tags(self):
+    return self._tags;
   def parent(self):
     return self._parent;
   def attributes(self):
     return self._attributes
+  # child add/remove
+  def attach(self, parent, update=False):
+    assert self._parent is None
+    self._parent = parent
+    self._parent._children.append(self)
+    if update:
+      self._index = len(parent._children)-1
+      for c in self.dfsiter():
+        c._level = c._parent._level+1
+  def detach(self, update=False):
+    assert self._parent and self._parent._children[self._index] == self
+    for sibling in self._parent._children[self._index+1:]:
+      sibling._index -= 1
+    del self._parent._children[self._index]
+    self._parent = None
+    if update:
+      self._index = 0
+      self._level = 0
+      for c in self.descendantiter():
+        c._level = c._parent._level+1
   # parser support
-  @staticmethod
-  def addchild(parent, child):
-    parent._children.append(child)
-    child._parent = parent
+  def addchild(self, child):
+    child.attach(self)
   # predicator
+  def identified(self):
+    return bool(self._identifier)
+  def tagged(self):
+    return bool(self._tags)
   def root(self):
     return not self._parent
   def leaf(self):
     return not self._children
   def intermediate(self):
     return self._parent and self._children
-  def identified(self):
-    return bool(self._identifier)
-  def labeled(self):
-    return bool(self._label)
-  def visible(self):
-    return self._visible
-  # transformer
-  def hide(self):
-    self._visible = False
-  def show(self):
-    self._visible = True
-  def asattr(self):
-    if not self._label:
-      raise AnonymousAttributeError(self)
-    self._parent._attributes[self._label] = self._text
-  def topology(self):
-    if not self._parent:
-      self._level = 0
-    elif self._parent._level >= 0:
-      self._level = self._parent._level+1
-    for i, c in enumerate(self._children):
-      c.index = i
   # iterator support
   def childiter(self):
     return iter(self._children)
-  def labeledchilditer(self):
-    return filter(lambda n: n._label, self._children)
-  def unlabeledchilditer(self):
-    return filter(lambda n: not n.l_abel, self._children)
-  def identifiedchilditer(self):
-    return filter(lambda n: n._identifier, self._children)
-  def unidentifiedchilditer(self):
-    return filter(lambda n: not n._identifier, self._children)
-  def visiblechilditer(self):
-    return filter(lambda n: n._visible, self._children)
-  def invisiblechilditer(self):
-    return filter(lambda n: not n._visible, self._children)
-  def ancestoriter(self):
-    node = self._parent
-    while node:
-      yield node
-      node = node._parent
   def siblingiter(self):
     if self._parent:
       for n in self._parent.children:
         if self != n:
           yield n
+  def ancestoriter(self):
+    node = self._parent
+    while node:
+      yield node
+      node = node._parent
   def bfsiter(self):
     current = [self]
     while current:
@@ -121,10 +110,25 @@ class Node:
         yield n
       next = itertools.chain.from_iterable(n.childiter() for n in current)
       current = list(next)
+  def descendantiter(self):
+    return itertools.chain.from_iterable(c.dfsiter() for c in self.childiter())
   def dfsiter(self):
     yield self
-    for n in itertools.chain.from_iterable(c.dfsiter() for c in self.childiter()):
+    for n in self.descendantiter():
       yield n
+  # update topology
+  def canonicalize(self):
+    if self._parent:
+      self._level = c._parent._level+1
+      self._index = self._parent._children.index(self)
+    else:
+      self._level = 0
+      self._index = 0
+    for c in self.descendantiter():
+      c._level = c._parent._level+1
+    for n in itertools.takewhile(lambda n: not n.leaf(), self.dfsiter()):
+      for i, c in enumerate(n._children):
+        c._index = i
 
 class Parser:
   nodepattern = r'''\A(?P<indent>[\s]*)     # (optional) leading spaces
@@ -178,145 +182,139 @@ class Parser:
       return [pseudoroot]
     return roots
 
-# class UndefinedGroupError(Exception):
-#   def __init__(self, group):
-#     self._group = group
-#   def __str__(self):
-#     return 'Undefined group ' + self._group
+class NonuniqueGroupError(Exception):
+  def __init__(self, node):
+    self._node = node
+  def __str__(self):
+    return 'Nonunique groups for node ' + str(node)
+class LinkNodeError(Exception):
+  def __init__(self, node):
+    self._node = node
+  def __str__(self):
+    return 'Invalide link node ' + str(node)
+
+class GVNode(Node):
+  def __init__(self, text, **kwargs):
+    super().__init__(text)
+    self._attrs = kwargs
+    self._edgeattrs = dict()
+    self._group=None
+  def attr(self, **kwargs):
+    self._attrs.update(kwargs)
+  def edgeattr(self, **kwargs):
+    self._edgeattrs.update(kwargs)
+class GVGroup(Node):
+  def __init__(self, name, **kwargs):
+    super().__init__(name)
+    self._attrs = kwargs
+    self._groupattrs = dict()
+    self._graph = None
+  def attr(self, **kwargs):
+    self._attrs.update(kwargs)
+  def groupattr(self, **kwargs):
+    self._groupattrs.update(kwargs)
+  def postdfsiter(self):
+    for n in itertools.chain.from_iterable(c.postdfsiter() for c in self.childiter()):
+      yield n
+    yield self
+  def commonancestor(self, other):
+    assert self != other
+    ancestors = set(self.ancestoriter())
+    for g in other.ancestoriter():
+      if g in ancestors:
+        return g
+    assert False
 
 class GraphvizBackend:
-  class Group(Node):
-    def __init__(self, name, graph):
-      super().__init__(name)
-      self.graph = graph
-    def subgroup(self, group):
-      self.addchild(self, group)
-    def bottomupiter(self):
-      for n in itertools.chain.from_iterable(c.bottomupiter()
-                                             for c in self.childiter()):
-        yield n
-      yield self
-  
-  def __init__(self, root, nodenamefn=Node.text, extralinkattrs=None, **kwargs):
-    self.root = root
-    self.nodenamefn = nodenamefn
-    if not extralinkattrs:
-      extralinkattrs = {
-        'style': 'dashed',
-        'weight': '0.1'}
-    self.extralinkattrs = extralinkattrs
-    self._gvoptions = kwargs
-    self._graph = gv.Graph(**self._gvoptions)
+  def __init__(self, root, **kwargs):
+    self._root = root
+    self._grouproot = GVGroup('grouproot', **kwargs)
     self._passes = []
     self._identifiednodes = dict()
-    self._grouproot = GraphvizBackend.Group('root', self._graph)
-    # predicate for groups, links and graphviz attributes
-    self._linkpred = lambda n: n.label() == 'link'
-    self._attrpred = self.patternmatcher(Node.label, r'\A[gne]\..+')
-
-  @staticmethod
-  def patternmatcher(nodefn, pattern, flags=0):
-    if not isinstance(pattern, re.Pattern):
-      pattern = re.compile(pattern, flags)
-    return lambda node: nodefn(node) and pattern.match(nodefn(node))
-  @staticmethod
-  def enumeratedmatcher(nodefn, eset):
-    if not isinstance(eset, set):
-      eset = set(eset)
-    return lambda node: nodefn(node) in eset
-
+    self.linkpredicate = lambda n: 'link' in n.tags()
+    self.nodenamefn = Node.text
+    self.extraedgeattrs = dict(weight='0.01', style='dashed', color='red')
   # grouping
   def group(self, nodes, name=None, parent=None, **kwargs):
     if parent is None:
       parent = self._grouproot
     if name is None:
-      name = 'anonymousgroup'+str(len(self._groups))
-    # graphattrs = self._gvoptions.copy()
-    # graphattrs.update(kwargs)
-    # subgraph = gv.Graph(name, **graphattrs)
-    subgraph = gv.Graph(name)
-    subgraph.attr(**kwargs)
-    subgroup = GraphvizBackend.Group(name, subgraph)
-    parent.subgroup(subgroup)
+      name = 'anongroup-{}-{}'.format(id(parent), len(parent._children))
+    group = GVGroup(name, **kwargs)
+    group.attach(parent)
     for n in nodes:
-      n.graph = subgraph
-    return subgroup
+      if n._group:
+        raise NonuniqueGroupError(n)
+      n._group = group
+    return group
   def groupsubtree(self, node, name=None, parent=None, **kwargs):
     return self.group(node.dfsiter(), name, parent, **kwargs)
-      
-  # group assemble op
-  def groupassembleop(self, group):
-    parent = group.parent()
-    if parent:
-      parent.graph.subgraph(group.graph)
-  # collect identifier op
-  def collectidentifiedop(self, node):
+
+  # group node op
+  def groupnodeop(self, node):
+    if not node._group:
+      node._group = self._grouproot
+  # create graph op
+  def creategraphop(self, group):
+    group._graph = gv.Graph(group.text(), **group._attrs)
+    group._graph.attr(**group._groupattrs)
+  # render node opeartion
+  def rendernodeop(self, node):
+    if self.linkpredicate(node):
+      return
+    name = self.nodenamefn(node)
+    label = None
+    if not self.nodenamefn is Node.text:
+      label = node.text()
+    assert node._group and node._group._graph
+    node._group._graph.node(name, label, **node._attrs)
+  # assemble graph op
+  def assemblegraphop(self, group):
+    if group.parent():
+      group.parent()._graph.subgraph(group._graph)
+  # render edge operation
+  def renderedgeop(self, node):
+    if not node.parent() or self.linkpredicate(node):
+      return
+    parent = node.parent()
+    headname = self.nodenamefn(node)
+    tailname = self.nodenamefn(parent)
+    # add edge to the root graph
+    self._grouproot._graph.edge(tailname, headname, **node._edgeattrs)
+  # collect node identifier op
+  def collectnodeidop(self, node):
     if node.identified():
       if node.identifier() in self._identifiednodes:
         raise RedefinedIdentifierError(node.identifier())
       self._identifiednodes[node.identifier()] = node
-  # hide attribute op
-  def nodeattrop(self, node):
-    if self._attrpred(node):
-      node.asattr()
-      node.hide()
-    # hide extra link nodes
-    if self._linkpred(node):
-      node.hide()
-  # extra link operation
-  def extralinkop(self, node):
-    if not self._linkpred(node):
+  # extra edge operation
+  def extraedgeop(self, node):
+    if not node.parent() or not self.linkpredicate(node):
       return
     if node.text() not in self._identifiednodes:
       raise UndefinedIdentifierError(node.text())
+    if node._children:
+      raise LinkNodeError(node)
     tail = node.parent()
     head = self._identifiednodes[node.text()]
-    if not tail.visible() or not head.visible():
-      return
-    edgeattrs = self.extralinkattrs.copy()
-    edgeattrs.update({k[2:]: v for k, v in node.attributes().items()
-                      if k.startswith('e.')})
     tailname = self.nodenamefn(tail)
     headname = self.nodenamefn(head)
-    self._graph.edge(tailname, headname, **edgeattrs)
-  # node render opeartion
-  def noderenderop(self, node):
-    if not node.visible():
-      return
-    name = self.nodenamefn(node)
-    label = node.text()
-    if self.nodenamefn is Node.text:
-      label = None
-    nodeattrs = {k[2:]: v for k, v in node.attributes().items()
-                 if k.startswith('n.')}
-    graph = node.graph
-    if graph is None:
-      graph = self._graph
-    graph.node(name, label, **nodeattrs)
-  # edge render operation
-  def edgerenderop(self, node):
-    parent = node.parent()
-    if parent and node.visible() and parent.visible():
-      edgeattrs = {k[2:]: v for k, v in node.attributes().items()
-                   if k.startswith('e.')}
-      name = self.nodenamefn(node)
-      parentname = self.nodenamefn(parent)
-      self._graph.edge(parentname, name, **edgeattrs)
+    # add edge to the root graph
+    attrs = self.extraedgeattrs.copy()
+    attrs.update(node._edgeattrs)
+    self._grouproot._graph.edge(tailname, headname, **attrs)
 
   def _populatestdpasses(self):
-    self._passes.append((self.collectidentifiedop, self.root.dfsiter()))
-    self._passes.append((self.nodeattrop, self.root.dfsiter()))
-    self._passes.append((self.noderenderop, self.root.dfsiter()))
-    self._passes.append((self.groupassembleop, self._grouproot.bottomupiter()))
-    self._passes.append((self.edgerenderop, self.root.dfsiter()))
-    self._passes.append((self.extralinkop, self.root.dfsiter()))
-  def render(self, prepasses=None, postpasses=None):
-    if prepasses:
-      self._passes.extend(prepasses)
+    self._passes.append((self.groupnodeop, self._root.dfsiter()))
+    self._passes.append((self.creategraphop, self._grouproot.dfsiter()))
+    self._passes.append((self.rendernodeop, self._root.dfsiter()))
+    self._passes.append((self.assemblegraphop, self._grouproot.postdfsiter()))
+    self._passes.append((self.renderedgeop, self._root.dfsiter()))
+    self._passes.append((self.collectnodeidop, self._root.dfsiter()))
+    self._passes.append((self.extraedgeop, self._root.dfsiter()))
+  def render(self):
     self._populatestdpasses()
-    if postpasses:
-      self._passes.extend(postpasses)
     for op, it in self._passes:
       for n in it:
         op(n)
-    return self._graph
+    return self._grouproot._graph
